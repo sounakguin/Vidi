6e8f122f984ca4e46525fb940a3bc892b8b8f911
Sounak Guin <sounak.guin@gmail.com>
2024-09-14 12:09:14 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['vidi-gamma.vercel.app'], 
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
